e16f36569348b1fa04e72375510f976c86538ac8
Maya <DahirowSumaia@gmail.com>
2024-07-09 19:58:28 -0500

    created AddToDoForm

--- todo/app/page.js
+++ todo/app/page.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Image from "next/image";
+import AddToDoForm from "./components/AddToDoForm";
 
 export default function Home() {
   return (
     <main className="b">
 <Header />
+<AddToDoForm />
 <Footer />
     </main>
   );
