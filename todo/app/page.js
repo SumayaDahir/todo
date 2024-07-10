1023b4c5c410be98a3853dfe74edb81faaf31014
Maya <DahirowSumaia@gmail.com>
2024-07-09 19:50:01 -0500

    made changes to the components

--- todo/app/page.js
+++ todo/app/page.js
@@ -1,9 +1,12 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-
+    <main className="b">
+<Header />
+<Footer />
     </main>
   );
 }
